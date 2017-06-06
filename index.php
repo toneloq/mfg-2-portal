83b21a5cb0f8944445a535c0a0a4a87fa5c321f0
mseeger <mseeger@mseeger-ltm.sfdcgomobile.salesforce.com>
2017-06-06 18:13:38 -0500

    Replaced the static site index.php page with the MVC Guts

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php
+
+require_once 'config/main.php';
+
+/**
+ * Will be registered as the __autoload() function
+ * @param type $class Class name to attempt to load
+ */
+function autoload($class) {
+    // if file does not exist in system folder
+    // then check in libs folder
+    if (file_exists('system/' . $class . ".php")) {
+        require_once 'system/' . $class . ".php";
+    } else {
+        require_once "lib/" . $class . ".php";
+    }
+    
+}
+
+spl_autoload_register('autoload');
+
+$app = new Bootstrap();
+
+?>
