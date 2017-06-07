8b38943617ef8657b65d88bee4c75d5b4ef9c14b
toneloq <tsaarnio+git@salesforce.com>
2017-06-06 23:25:03 -0500

    Update index.php

--- views/index.php
+++ views/index.php
@@ -1,3 +1,3 @@
 <div class="banner">
-	<img src="https://mfgqtc--c.na40.content.force.com/servlet/servlet.ImageServer?id=01546000000NZIK&oid=00D460000000MPU&lastMod=1496805381000" href=""> 
+	 
 </div>
